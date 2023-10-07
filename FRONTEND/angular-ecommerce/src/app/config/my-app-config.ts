e68caa0516d8e25b72534443f2bf8539cda5c46f
palarnab0707 <79499723+palarnab0707@users.noreply.github.com>
2023-10-07 15:20:20 +0530

    id hide

--- FRONTEND/angular-ecommerce/src/app/config/my-app-config.ts
+++ FRONTEND/angular-ecommerce/src/app/config/my-app-config.ts
@@ -1,7 +1,7 @@
 export default {
     oidc : {
-        clientId : '0oabm0fvlhLjazC2Q5d7',
-        issuer : 'https://dev-21960644.okta.com/oauth2/default',
+        clientId : '<Id>',
+        issuer : 'https://<domain>/oauth2/default',
         redirectUri : 'https://localhost:4200/login/callback',
         scopes : ['openid', 'profile', 'email']
     }
